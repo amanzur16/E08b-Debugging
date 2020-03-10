517686d5ee9f913b0a8b1b091c929f8dc8583b3d
amanzur16 <59851844+amanzur16@users.noreply.github.com>
2020-03-09 23:31:01 -0400

    changes

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -8,8 +8,10 @@ export var score: = 100
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	picked()
+	
 
 
 func picked() -> void:
 	PlayerData.score += score
 	anim_player.play("picked")
+	score += 100
